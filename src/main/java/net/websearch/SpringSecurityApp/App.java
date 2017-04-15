f393149d18fa10757eeaec88aa24ed26bebc426a
Kamikadze9xdd <nazar.kulik99@gmail.com>
2017-04-15 09:14:59 +0300

    Our new login and register form

---
+++ src/main/java/net/websearch/SpringSecurityApp/App.java
@@ -0,0 +1,13 @@
+package net.websearch.SpringSecurityApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
